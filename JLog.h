be373319b477ba0a1381bba85b03b75b482b3a68
John Sandstedt <admin@aggendo.byethost31.com>
2017-09-23 22:37:40 -0400

    Revert "finally got the damn header writing code to work"
    This reverts commit 5770bfa49751d126909b51ab95a8d1bd44868140.

--- JLog.h
+++ JLog.h
@@ -1,34 +1,29 @@
 #ifndef JLog_h
 #define JLog_h
 
-#define DEBUG
-
 class JLog
 {
   public:
     JLog();
-    bool begin(char *filename, int sdcardId, bool overWrite);
-    bool begin(char *filename, int sdcardId);
-    void writeHeader();
-    void addValueId(char id, char *Name);
-    void addToHeader(char *Name, char *ValueFor);
+    bool begin(char *filename, char sdcardId, bool overWrite);
+    bool begin(char *filename, char sdcardId);
+    void addSensorLogId(char id, char *Name);
+    void addToHeader(char *Name, char *Value);
     //void setIdByteSize(int Size);
+    bool writeHeader();
     bool writeValue(char sensorId, int sensorValue);
     bool writeValue(char sensorId, float sensorValue);
     bool writeValue(char sensorId, long sensorValue);
     bool writeValue(char sensorId, char sensorValue);
     bool writeValue(char sensorId, short sensorValue);
     bool writeValue(char sensorId, double sensorValue);
-    #ifdef DEBUG
-    void serialUpload(); //for debugging uploads file through serial
-    #endif
   private:
     int _IdByteSize;
-    //in the below method finalWrite denotes that the output file is _filename
-    void addToFile(bool finalWrite, int line, char* Entry);
     char* _filename; //TODO: 50 character limit on filename
+    int _sdcardId;
+    char* _headercontents;
+    char* _sensorIdNames;
     #define ENDBYTE 0xff
     #define FILE_VERSION "0.1"
 };
 #endif
-
